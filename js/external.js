ed8e3776c2feb01c581d180651c121c9d0272dd2
timvarjan <127249482+timvarjan@users.noreply.github.com>
2023-03-13 09:15:45 -0500

    Update from transition

--- js/external.js
+++ js/external.js
@@ -2,32 +2,55 @@
 
 console.log("Hello from inline JavaScript");
 
-// alert("Welcome to my Website!");
+// // alert("Welcome to my Website!");
+// //
+// // var colorChoice = prompt("What is your favorite color?");
 //
-// var colorChoice = prompt("What is your favorite color?");
+// alert("Great, " + colorChoice + " is my favorite color too!");
+//
+//
+// var dailyCharge = 3
+//
+// var littleMermaid = prompt("How many days did you rent The Little Mermaid for?") * dailyCharge;
+//
+// alert("The cost for this rental is $" + littleMermaid);
+//
+// var brotherBear = prompt("How many days did you rent The Brother Bears for?") * dailyCharge;
+//
+// alert("The cost for this rental is $" + brotherBear);
+//
+// var hercules = prompt("How many days did you rent Hercules for?") * dailyCharge;
+//
+// alert("The cost for this rental is $" + hercules);
+//
+// var grandTotal = (littleMermaid + brotherBear + hercules);
+//
+// var price = alert("The total amount you have to pay is $" + grandTotal);
+//
+// console.log(typeof grandTotal);
 
-alert("Great, " + colorChoice + " is my favorite color too!");
+// var google = prompt("How many hours did you work at Google this week?") * 400;
+// var amazon = prompt("How many hours did you work at Amazon this week?") * 380;
+// var facebook = prompt("How many hours did you work at Facebook this week?") * 350;
+//
+// alert("You earned a total of $" + (google + amazon + facebook) + " this week!");
+
+// var totalClassSeats = +prompt("What is the maximum capacity of the class?");
+// var currentlyEnrolled = +prompt("How many students are currently enrolled?");
+// var openSeats = (totalClassSeats - currentlyEnrolled);
+// var classDay = prompt("Which day of the week is the class held?");
+// var availability = ["monday", "wednesday"];
+// var checkSchedule = availability.includes(classDay.toLowerCase())
+// console.log(checkSchedule)
+// var isElegible = openSeats > 0 && checkSchedule;
+// console.log(isElegible);
+// alert("Eligeble to enroll: " + isElegible);
 
 
-var dailyCharge = 3
 
-var littleMermaid = prompt("How many days did you rent The Little Mermaid for?") * dailyCharge;
 
-alert("The cost for this rental is $" + littleMermaid);
 
-var brotherBear = prompt("How many days did you rent The Brother Bears for?") * dailyCharge;
 
-alert("The cost for this rental is $" + brotherBear);
-
-var hercules = prompt("How many days did you rent Hercules for?") * dailyCharge;
-
-alert("The cost for this rental is $" + hercules);
-
-var grandTotal = (littleMermaid + brotherBear + hercules);
-
-var price = alert("The total amount you have to pay is $" + grandTotal);
-
-console.log(typeof grandTotal);
 
 
 
