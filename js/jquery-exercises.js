28d8c428c34d09216209d286b9ff929031dcf27c
timvarjan <127249482+timvarjan@users.noreply.github.com>
2023-04-19 12:45:12 -0300

    separated  (h1, li, p) selectors

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -7,7 +7,10 @@
 // $('.codeup').css('border', '1px solid #F00');
 
 //element/multiple selectors***
+// $('h1').css('background-color', 'green')
+// $('li').css('background-color', 'green')
+// $('p').css('background-color', 'green')
 $("li").css('font-size', '20px');
 $("h1, li, p").css('background-color', 'orange')
 var contents = $('h1').html();
-alert(contents);
+// alert(contents);
