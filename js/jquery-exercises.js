a6287bf48963b5f4054ed74372943285deea70eb
timvarjan <127249482+timvarjan@users.noreply.github.com>
2023-04-19 12:26:43 -0300

    completed js-query exercise

---
+++ js/jquery-exercises.js
@@ -0,0 +1,13 @@
+//id selectors***
+// $(".container").css('background-color', 'orange');
+// var contents = $('#item-1').html();
+// alert(contents);
+
+//class selectors***
+// $('.codeup').css('border', '1px solid #F00');
+
+//element/multiple selectors***
+$("li").css('font-size', '20px');
+$("h1, li, p").css('background-color', 'orange')
+var contents = $('h1').html();
+alert(contents);
